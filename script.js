2a2c850ed76b48f9056e2f144b930650c61d6d9c
tobuya <thomasobuya95@yahoo.com>
2022-03-18 21:48:54 +0300

    UPDATE: Ensure square new grids are created
    Send a popup asking for the number of squares per side of the new grid.
    The new grid should be generated in the same total space as before.
    We need this so that we have got a new sketch pad.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const container = document.querySelector('#grid-container');
 const aside = document.querySelector('#aside');
 
 window.addEventListener('load', () => {
-    container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, minmax(0,1fr)); grid-column-gap: 2px;');
+    container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, minmax(0,1fr));');
     let squared = 256;
     for (let i = 0; i < 256; i++){
         const div = document.createElement('div');
-        div.setAttribute('style', 'border: 1px solid red;')
+        div.setAttribute('style', 'border: 0.5px dotted white; width: 100%; height: 100%;')
         container.appendChild(div);
         div.addEventListener('click', () => {
             div.style.backgroundColor= 'aqua';
@@ -14,25 +14,13 @@ window.addEventListener('load', () => {
     }
 });
 
-/*const divNumber = 256;
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid-items');
-    div.innerHTML= i + 1;
-    container.appendChild(div);
-
-    div.addEventListener("click", () => {
-        div.style.backgroundColor= "aqua";
-    })
-}*/
-
 const resetButton = document.createElement('button');
 resetButton.textContent = 'RESET';
 resetButton.classList.add('button');
 aside.appendChild(resetButton);
 resetButton.addEventListener('click', () => {
     container.replaceChildren();
-    let squarePerSide = prompt('How many squares per side?(Maximum: 100)',0);
+    let squarePerSide = prompt('How many squares per side?(Maximum: 100)',16);
     let perSide = Number(squarePerSide);
     container.setAttribute('style',`display: grid; grid-template-columns: repeat(${perSide}, 1fr);`)
     if (perSide <= 0 || isNaN(perSide)){
@@ -43,21 +31,24 @@ resetButton.addEventListener('click', () => {
         let squareDiv = (perSide * perSide);
         for (let j = 0; j < squareDiv; j++){
             const gridDiv = document.createElement('div');
-            gridDiv.setAttribute('style', 'border: 1px solid black; margin: 5px;');
+            gridDiv.setAttribute('style', 'border: 0.5px dotted white; width: 100%; height: 100%;');
             container.appendChild(gridDiv);
+            gridDiv.addEventListener('click', () => {
+                gridDiv.style.background = 'aqua';
+            })
         }
     }
 });
 
 const colorButton = document.createElement('button');
-colorButton.textContent = 'COLOR MODE';
+colorButton.textContent = 'COLOR';
 colorButton.classList.add('button');
 aside.appendChild(colorButton);
 //colorButton.addEventListener('click', )
 
 
 const rainbowButton = document.createElement('button');
-rainbowButton.textContent = 'RAINBOW MODE';
+rainbowButton.textContent = 'RAINBOW';
 rainbowButton.classList.add('button');
 aside.appendChild(rainbowButton);
 
